973318a6c2e09396dafa3c87a2a120ab7a8fda69
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-02-06 22:12:54 -0300

    creating binary build

--- builds/binary_build.py
+++ builds/binary_build.py
@@ -4,12 +4,12 @@ from platform import system as platform
 
 if platform() == 'Windows':
     #build the binary
-    system('pyinstaller  --onefile --name=Aurora.exe  ../aurora/run.py')
+    system('pyinstaller  --onefile --name=Aurora.exe  ../aurora/main.py')
     shutil.move('dist/Aurora.exe', '../outputs/binarys/Aurora.exe')
 
 if  platform() == 'Linux':
     #build the binary
-    system('pyinstaller  --onefile --name=AuroraLinux  ../aurora/run.py')
+    system('pyinstaller  --onefile --name=AuroraLinux  ../aurora/main.py')
     shutil.move('dist/AuroraLinux', '../outputs/binarys/Aurora')
 
 #remove the build folder
