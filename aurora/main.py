06282f1b540943068efce9c1217f0bddbd3af2a1
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-02-06 23:15:14 -0300

    at

--- aurora/main.py
+++ aurora/main.py
@@ -3,6 +3,7 @@
 import os 
 from aurora.entrys import get_entrys
 from aurora.execution import generate_repository_actions
+from aurora.general import print_if_not_quiet
 import multiprocessing
 from sys import exit
 
@@ -18,9 +19,9 @@ def main():
     quiet = entrys['quiet']
     respositorys = entrys['repositorys']
     #runs "generate_repository_actions" in diferents subprocesses
-    
+    print_if_not_quiet(quiet, 'Starting Project Aurora...')
     for respository_props in respositorys:
-        
+        print_if_not_quiet(quiet, 'Starting repository: ' + respository_props['name'])
         p = multiprocessing.Process(target=generate_repository_actions, args=(respository_props, quiet))
         
         p.start()
