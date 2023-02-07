791535c01179c19610844eacf05188d63fd893b3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-02-06 23:27:55 -0300

    at

--- binary_build.py
+++ binary_build.py
@@ -2,15 +2,19 @@ from os import system,remove,makedirs
 import shutil
 from platform import system as platform
 
+shutil.rmtree('build', ignore_errors=True)
+shutil.rmtree('dist', ignore_errors=True)
 
 
 if platform() == 'Windows':
     #build the binary
-    system('pyinstaller  --onefile --name=Aurora.exe  aurora/main.py')
-    shutil.move('dist/Aurora.exe', 'outputs/binarys/Aurora.exe')
+    system('pyinstaller  --onefile  aurora/main.py')
+    shutil.move('dist/main.exe', 'outputs/binarys/Aurora.exe')
 
 if  platform() == 'Linux':
     #build the binary
-    system('pyinstaller  --onefile --name=AuroraLinux  aurora/main.py')
-    shutil.move('dist/AuroraLinux', 'outputs/binarys/Aurora')
+    system('pyinstaller --onefile aurora/main.py')
+    shutil.move('dist/main', 'outputs/binarys/Aurora')
 
+shutil.rmtree('build', ignore_errors=True)
+shutil.rmtree('dist', ignore_errors=True)
